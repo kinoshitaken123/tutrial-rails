a3d546a70f5962927d2efcdf7232cfaae5979101
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-18 12:43:39 +0000

    [User.model create]

--- generate/config/routes.rb
+++ generate/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
+
+  get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
