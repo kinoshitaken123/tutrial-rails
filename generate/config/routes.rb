f76113db3654226d38b4f32d52adf93fb296a8b3
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-22 01:57:27 +0000

    [make form]

--- generate/config/routes.rb
+++ generate/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   get  '/login',   to: 'sessions#new'
   get  '/login',   to: 'sessions#create'
-  delete '/logout',to: 'sessions#destroyp'
+  delete '/logout',to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :users
