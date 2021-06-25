8699572a724f493131002e508681d74f13578d41
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-25 13:07:06 +0000

    [Add _header.html.erb]

--- generate/app/controllers/sessions_controller.rb
+++ generate/app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -15,4 +17,4 @@ class SessionsController < ApplicationController
 
   def destroy
   end
-end
+endP
