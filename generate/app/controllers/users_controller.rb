d2368302e3b5012ff5216b996ff94429145c6428
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-30 12:07:01 +0000

    [Add will_paginate gem]

--- generate/app/controllers/users_controller.rb
+++ generate/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action:logged_in_user,only:[:index,:edit,:update]
+  before_action:logged_in_user,only:[:index,:edit,:update, :destroy]
   before_action:correct_user,only:[:edit,:update]
   
   # GET /users
@@ -42,6 +42,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:seccer] = "User deleted"
+    redirect_to users_url
+  end  
   private
   
   def user_paramas
--- generate/test/controllers/users_controller_test.rb
+++ generate/test/controllers/users_controller_test.rb
@@ -31,12 +31,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
+  test "should redirect update when not logged in" do
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: FILL_IN } }
+    assert_not @other_user.FILL_IN.admin?
   end
   
   test "should redirect index when not logged in" do
