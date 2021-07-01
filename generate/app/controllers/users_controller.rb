eb3a00c13f062d8e0dfddc010ea5649c6c0df5cf
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-07-01 13:01:51 +0000

    [Finish ch10 admin]

--- generate/app/controllers/users_controller.rb
+++ generate/app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action:logged_in_user,only:[:index,:edit,:update, :destroy]
   before_action:correct_user,only:[:edit,:update]
+  before_action :admin_user, only: :destroy
   
+
   # GET /users
   def index
     @users = User.User.paginate(page: params[:page])
@@ -62,9 +64,13 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end  
-  
+  #正しいユーザーか確認する
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless @user==current_user?(@user)
+  end  
+  #管理者かどうか確認する
+  def admin_user
+    redirect_to(root_url) unless current_user .admin?
   end  
 end
